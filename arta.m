38868b52f80d22285d8208d2098cc94ac9d663a4
arta fallahpoor <102357381+artafp@users.noreply.github.com>
2023-12-29 22:00:10 +0330

    Update arta.m

--- arta.m
+++ arta.m
@@ -1,41 +1,31 @@
 clc
 clear all
 syms x y z
-
-f1 = function1()
-gradient_f = gradient(f1, [x, y, z])
-hessian_f = hessian(f1, [x, y, z])
-n=3
-critical_points = solve(gradient_f == 0, [x, y, z])
-
-Q=hessian_f;
-Q=Q'*Q+10.^(-5)*eye(n);
-b=rand(n,1);
-k=10 ;%iteration ;
-ep=10^-5;
-x0=rand(n,1);
-f=@(x)1/2*x'*Q*x+b'*x;
-
-re=[];
+f1 = function1();
+gradient_f = gradient(f1, [x, y, z]);
+hessian_f = hessian(f1, [x, y, z]);
+eps=2*10^-3;
 err=1;
-i=0;
-while err>ep
-% for i=1:k
-%     gk=Q*x0+b;
-%     alp=gk'*gk/(gk'*Q*gk);
-%     x1=x0-alp*gk;
-%     re=[re; i f(x1)-f(x0)];
-%     x0=x1;
-% end
- gk=Q*x0+b;
- alp=1;s=3/4;eps=0.9;
-
-     while f(x0-alp*gk)>f(x0)-eps*alp*gk'*gk
-        alp=s*alp;
-     end
-    x1=x0-alp*gk;
-    i=i+1;
-    err=norm(x1-x0);
-    re=[re; i err f(x1)-f(x0)];
-    x0=x1;
+x0=[10,10,10];
+while eps<err
+    A = hessian_f;
+    Hessian_at_point = double(subs(hessian_f, [x, y, z], x0));
+    Gradient_at_point = double(subs(gradient_f, [x, y, z], x0));
+    % بررسی مثبت معین بودن با استفاده از chol
+    try
+        chol(Hessian_at_point);
+        disp('ماتریس هسیان معین مثبت است.');
+    catch
+        error('ماتریس هسیان مثبت معین نیست یا خطا در بررسی رخ داده است.');
+    end
+    if det(Hessian_at_point) ~= 0
+        A_inv = inv(Hessian_at_point); % محاسبه ماتریس وارون اگر ماتریس قابل معکوس باشد
+        % اکنون ماتریس وارون A_inv در دسترس شماست
+    else
+        error('ماتریس وارون وجود ندارد، زیرا ماتریس قابل معکوس نیست.');
+    end
+    x1 = x0'-(A_inv*Gradient_at_point);
+    err=sqrt(norm(x1-x0'));
+    x0=x1';
+    disp(err)
 end
