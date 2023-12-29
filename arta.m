513733cf4224c0ef1337d7149a95ebf4708ceabd
arta fallahpoor <102357381+artafp@users.noreply.github.com>
2023-12-29 22:39:46 +0330

    Update arta.m

--- arta.m
+++ arta.m
@@ -4,9 +4,10 @@ syms x y z
 f1 = function1();
 gradient_f = gradient(f1, [x, y, z]);
 hessian_f = hessian(f1, [x, y, z]);
-eps=2*10^-3;
+eps=1*10^-1000;
 err=1;
-x0=[10,10,10];
+x0=[1,1,1];
+i=1;
 while eps<err
     A = hessian_f;
     Hessian_at_point = double(subs(hessian_f, [x, y, z], x0));
@@ -21,11 +22,13 @@ while eps<err
     if det(Hessian_at_point) ~= 0
         A_inv = inv(Hessian_at_point); % محاسبه ماتریس وارون اگر ماتریس قابل معکوس باشد
         % اکنون ماتریس وارون A_inv در دسترس شماست
+        disp('ماتریس هسیان وارون پذیر است.');
     else
         error('ماتریس وارون وجود ندارد، زیرا ماتریس قابل معکوس نیست.');
     end
     x1 = x0'-(A_inv*Gradient_at_point);
     err=sqrt(norm(x1-x0'));
+    i=i+1;
     x0=x1';
     disp(err)
 end
